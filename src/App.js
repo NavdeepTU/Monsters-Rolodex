c70c16563ac6a83940bc9c28071329a165b7a72d
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-07-07 08:02:25 +0530

    map and fetching from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react'
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string:"Navdeep"
+      monsters:[]
     }
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response =>  response.json())
+    .then(users => this.setState({monsters:users}))
+  }
+
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{this.state.string}</p>
-        <button onClick={() => this.setState({string:"Hello Navdeep"})}>Change Message</button>
-      </header>
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        }
     </div>
     )
   }
