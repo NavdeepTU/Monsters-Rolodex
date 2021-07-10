ae1e1a58e15cfad9438f8bf1d71e6271f15dabb5
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-07-10 13:05:58 +0530

    added card-list component with initial styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-import './App.css';
+import { CardList } from "./components/card-list/card-list.component";
+
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters:[]
-    }
+      monsters: [],
+    };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response =>  response.json())
-    .then(users => this.setState({monsters:users}))
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-        }
-    </div>
-    )
+        <CardList name="Navdeep">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
+      </div>
+    );
   }
 }
 
